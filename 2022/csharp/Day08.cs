a155543950df6619e4309ffea27a90e28af912c9
Danillo Magno <danillo.magno@zbra.dev>
2022-12-10 09:20:28 -0300

    Day08 and Day09

---
+++ 2022/csharp/Day08.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+
+class Day08 : Puzzle
+{
+    protected override string Part1(string[] inputLines)
+    {
+        var trees = inputLines;
+
+        var size = trees.Length;
+
+        var result = 0;
+
+        bool IsVisible(string[] trees, int i, int j)
+        {
+            var tree = trees[i][j];
+
+            var isVisible = true;
+            for (var k = j - 1; k >= 0; k--) // left
+            {
+                isVisible &= tree > trees[i][k];
+            }
+            if (isVisible) return true;
+
+            isVisible = true;
+            for (var k = j + 1; k < size; k++) // right
+            {
+                isVisible &= tree > trees[i][k];
+            }
+            if (isVisible) return true;
+
+            isVisible = true;
+            for (var k = i - 1; k >= 0; k--) // up
+            {
+                isVisible &= tree > trees[k][j];
+            }
+            if (isVisible) return true;
+
+            isVisible = true;
+            for (var k = i + 1; k < size; k++) // down
+            {
+                isVisible &= tree > trees[k][j];
+            }
+
+            return isVisible;
+        }
+
+        for (var i = 1; i < size - 1; i++)
+        {
+            for (var j = 1; j < size - 1; j++)
+            {
+                if (IsVisible(trees, i, j)) result++;
+            }
+        }
+
+        return (result + (4 * size) - 4).ToString();
+    }
+
+    protected override string Part2(string[] inputLines)
+    {
+        var trees = inputLines;
+
+        var size = trees.Length;
+
+        var result = 0;
+
+        for (var i = 1; i < size - 1; i++)
+        {
+            for (int j = 1; j < size - 1; j++)
+            {
+                int k;
+                int count;
+                var scenicScore = 1;
+
+                var tree = trees[i][j];
+
+                for (k = j - 1, count = 0; k >= 0; k--) // left
+                {
+                    count++;
+                    if (tree <= trees[i][k]) break;
+                }
+                scenicScore *= count;
+
+                for (k = j + 1, count = 0; k < size; k++) // right
+                {
+                    count++;
+                    if (tree <= trees[i][k]) break;
+                }
+                scenicScore *= count;
+
+                for (k = i - 1, count = 0; k >= 0; k--) // up
+                {
+                    count++;
+                    if (tree <= trees[k][j]) break;
+                }
+                scenicScore *= count;
+
+                for (k = i + 1, count = 0; k < size; k++) // down
+                {
+                    count++;
+                    if (tree <= trees[k][j]) break;
+                }
+                scenicScore *= count;
+
+                result = Math.Max(result, scenicScore);
+            }
+        }
+
+        return result.ToString();
+    }
+}
---
+++ 2022/csharp/Day09.cs
@@ -0,0 +1,135 @@
+﻿class Day09 : Puzzle
+{
+    protected override string Part1(string[] inputLines)
+    {
+        return TailCount(inputLines, new Rope(length: 2));
+    }
+
+    private static string TailCount(string[] inputLines, Rope rope)
+    {
+        foreach (var line in inputLines)
+        {
+            var movement = Rope.Create(movement: line);
+
+            rope.Move(movement);
+        }
+
+        return rope.TailCount().ToString();
+    }
+
+    protected override string Part2(string[] inputLines)
+    {
+        return TailCount(inputLines, new Rope(length: 10));
+    }
+}
+
+enum Direction
+{
+    Up = 'U',
+    Down = 'D',
+    Left = 'L',
+    Right = 'R'
+}
+
+record Pos
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+
+    public double Distance(Pos other)
+    {
+        return Math.Sqrt(Math.Pow(X - other.X, 2) + Math.Pow(Y - other.Y, 2));
+    }
+}
+
+record Movement(Direction Direction, int Steps);
+
+class Rope
+{
+    private static readonly int[] units = { 0, 1, -1 };
+
+    private readonly List<Pos> knots = new();
+
+    private Pos Head => knots.First();
+    private Pos Tail => knots.Last();
+
+    private readonly HashSet<Pos> tailsPositions = new()
+    {
+        new Pos { X = 0, Y = 0 }
+    };
+
+    public Rope(int length = 2)
+    {
+        for (var i = 0; i < length; i++)
+        {
+            knots.Add(new Pos { X = 0, Y = 0 });
+        }
+    }
+
+    public void Move(Movement movement)
+    {
+        for (var i = 0; i < movement.Steps; i++)
+        {
+            MoveHead(movement.Direction);
+
+            for (int j = 1; j < knots.Count; j++)
+            {
+                MoveKnot(knots[j], knots[j - 1]);
+            }
+
+            tailsPositions.Add(Tail);
+        }
+    }
+
+    public int TailCount() => tailsPositions.Count;
+
+    public static Movement Create(string movement)
+    {
+        var tokens = movement.Split(" ");
+
+        return new Movement((Direction)tokens[0][0], int.Parse(tokens[1]));
+    }
+
+    private void MoveHead(Direction direction)
+    {
+        var _ = direction switch
+        {
+            Direction.Up => Head.Y++,
+            Direction.Down => Head.Y--,
+            Direction.Left => Head.X--,
+            Direction.Right => Head.X++,
+            _ => throw new NotImplementedException(),
+        };
+    }
+
+    private static void MoveKnot(Pos knot, Pos reference)
+    {
+        if (IsTouching(knot, reference)) return;
+
+        if (reference.X == knot.X)
+        {
+            knot.Y += Math.Sign(reference.Y - knot.Y);
+        }
+        else if (reference.Y == knot.Y)
+        {
+            knot.X += Math.Sign(reference.X - knot.X);
+        }
+        else if (reference.Distance(knot) > 1)
+        {
+            knot.Y += Math.Sign(reference.Y - knot.Y);
+            knot.X += Math.Sign(reference.X - knot.X);
+        }
+    }
+
+    private static bool IsTouching(Pos knot, Pos reference)
+    {
+        foreach (var x in units)
+        {
+            foreach (var y in units)
+            {
+                if (knot.X + x == reference.X && knot.Y + y == reference.Y) return true;
+            }
+        }
+        return false;
+    }
+}
